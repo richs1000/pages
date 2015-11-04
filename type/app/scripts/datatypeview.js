9951d099dfdfdf7a291b5fc5fb9b7886bf720d33
vinicustod <vinihcius.custodio@gmail.com>
2015-11-04 14:45:21 -0500

    Question 4 - Datatypes

---
+++ type/app/scripts/datatypeview.js
@@ -0,0 +1,21 @@
+function DatatypeView(_simView) {
+	// keep a link to the view
+	this.simView = _simView;
+}
+
+
+/*
+<table id="fringeTable">
+	<tr>
+		<th>Row #</th>
+		<th>Node</th>
+		<th>Order Added</th>
+		<th>Total Cost</th>
+		<th>Heuristic</th>
+	</tr>
+	<tr>
+	</tr>
+</table>
+*/
+
+
